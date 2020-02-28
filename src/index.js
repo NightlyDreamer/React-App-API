d9aee9afaf098865eeccf5a1f9029bcbfb2eafde
Vlad <NightlyDreamer@users.noreply.github.com>
2020-02-28 13:39:47 +0200

    add fetch api Error processing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// => fetch(URL)=> promis(resolve).then((res) => { return res.json } ).then( (body) => {console.log(body)})
 
-const App = () => {
-    return <h1>Hello World</h1>
+const getResource = async(url) => {
+    const resolve = await fetch(url);
+    
+    if(!resolve.ok){
+        throw new Error(`Could not fetch ${url}, received ${resolve.status}`)
+    }
+
+    return await resolve.json();  
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+getResource('https://swapi.co/api/people/123123123/')
+.then((body) => {
+    console.log (body);
+})
+.catch((err) => {
+    console.log(err)
+})
